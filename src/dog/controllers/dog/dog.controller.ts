e04ef771e2637241a30b6aa179ebffd1ca54bce6
Jesus Sanchez Esaa <jesus.sanchez@dualbootpartners.com>
2023-03-20 14:44:49 -0400

    feat: practice-4

--- src/dog/controllers/dog/dog.controller.ts
+++ src/dog/controllers/dog/dog.controller.ts
@@ -10,21 +10,57 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOkResponse,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { CreateDogDto } from 'src/dog/dto/create-dog.dto';
+import { DogDto } from 'src/dog/dto/dog.dto';
 import { UpdateDogDto } from 'src/dog/dto/update-dog.dto';
 import { DogService } from '../../services/dog/dog.service';
 
+@ApiTags('Dog')
 @Controller('dog')
 export class DogController {
   constructor(private readonly dogService: DogService) {}
 
   @Get(':dogId')
+  @ApiParam({
+    name: 'dogId',
+    type: Number,
+    description: 'The id of the dog',
+    required: true,
+  })
+  @ApiOkResponse({
+    description: 'The dog data',
+    type: DogDto,
+  })
   findOne(@Param('dogId') dogId: number) {
     return this.dogService.findOne(dogId);
   }
 
   @Get()
+  @ApiQuery({
+    name: 'breed',
+    type: String,
+    description: 'The breed of the dog',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'age',
+    type: Number,
+    description: 'The age of the dog',
+    required: false,
+  })
+  @ApiOkResponse({
+    description: 'The dogs records',
+    type: DogDto,
+    isArray: true,
+  })
   find(@Query('breed') breed: string, @Query('age') age: number) {
     if (age || breed) {
       return this.dogService.find(breed, age);
@@ -33,18 +69,34 @@ export class DogController {
   }
 
   @Post()
+  @ApiBody({
+    type: CreateDogDto,
+    description: 'The data to create dog',
+    required: true,
+  })
   @UsePipes(ValidationPipe)
   create(@Body() createDogDto: CreateDogDto) {
     return this.dogService.create(createDogDto);
   }
 
   @Patch(':dogId')
+  @ApiBody({
+    type: UpdateDogDto,
+    description: 'The data to update dog',
+    required: true,
+  })
   @UsePipes(ValidationPipe)
   update(@Body() updateDogDto: UpdateDogDto, @Param('dogId') dogId: number) {
     return this.dogService.update(updateDogDto, dogId);
   }
 
   @Delete(':dogId')
+  @ApiParam({
+    name: 'dogId',
+    type: Number,
+    description: 'The id of the dog',
+    required: true,
+  })
   remove(@Param('dogId') id: string) {
     return this.dogService.delete(+id);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs Learning')
+    .setVersion('1.0')
+    .addTag('nest-learning')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
